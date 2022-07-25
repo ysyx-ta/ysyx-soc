81e2f2778b893bb8ad996b9238a6e10756d99e53
Mei-x-l <you@example.com>
2022-07-25 03:34:38 -0700

    64

--- S011HD1P_X32Y2D128.v
+++ S011HD1P_X32Y2D128.v
@@ -2,10 +2,10 @@ module S011HD1P_X32Y2D128(
     Q, CLK, CEN, WEN, A, D
 );
 parameter Bits = 128;
-parameter Word_Depth = 256;
-parameter Add_Width = 8;
+parameter Word_Depth = 64;
+parameter Add_Width = 6;
 
-output  reg [Bits-1:0]  Q;
+output  reg [Bits-1:0]      Q;
 input                   CLK;
 input                   CEN;
 input                   WEN;
@@ -14,11 +14,10 @@ input   [Bits-1:0]      D;
 
 reg [Bits-1:0] ram [0:Word_Depth-1];
 always @(posedge CLK) begin
-    if(WEN) begin
+    if(!CEN && !WEN) begin
         ram[A] <= D;
     end
+    Q <= !CEN && WEN ? ram[A] : {4{$random}};
 end
-
-assign Q = CEN ? ram[A] : {4{$random}};
 
 endmodule
--- S011HD1P_X32Y2D128_BW.v
+++ S011HD1P_X32Y2D128_BW.v
@@ -2,8 +2,8 @@ module S011HD1P_X32Y2D128_BW(
     Q, CLK, CEN, WEN, BWEN, A, D
 );
 parameter Bits = 128;
-parameter Word_Depth = 256;
-parameter Add_Width = 8;
+parameter Word_Depth = 64;
+parameter Add_Width = 6;
 parameter Wen_Width = 128;
 
 output reg [Bits-1:0] Q;
@@ -14,16 +14,16 @@ input [Wen_Width-1:0] BWEN;
 input [Add_Width-1:0] A;
 input [Bits-1:0]      D;
 
-wire cen  = CEN;
-wire wen  = WEN;
-wire [Wen_Width-1:0] bwen = BWEN;
+wire cen  = ~CEN;
+wire wen  = ~WEN;
+wire [Wen_Width-1:0] bwen = ~BWEN;
 
 reg [Bits-1:0] ram [0:Word_Depth-1];
 always @(posedge CLK) begin
-    if(wen) begin
+    if(cen && wen) begin
         ram[A] <= (D & bwen) | (ram[A] & ~bwen);
     end
-    Q <= cen ? ram[A] : {4{$random}};
+    Q <= cen && !wen ? ram[A] : {4{$random}};
 end
 
 endmodule
